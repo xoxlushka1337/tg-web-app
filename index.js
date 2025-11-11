10859a602c47c9695f7d014653b20116fb7c1478
uliana <ulana.xoxlushka@gmail.com>
2025-11-11 19:37:31 +0300

    fix webAppUrl

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const TelegramBot = require('node-telegram-bot-api')
 
 // replace the value below with the Telegram token you receive from @BotFather
 const token = '7998383392:AAGCFyPsqQEC9mMPBzCpqSHQFS170HBbq6I'
-const webAppUrl = 'https://tgweb-app1.netlify.app/'
+const webAppUrl = 'https://tgweb-app1.netlify.app'
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, { polling: true })
 
@@ -18,13 +18,13 @@ bot.on('message', async msg => {
     })
   }
 
-  if (msg.web_app_data?.data) {
+  if (msg?.web_app_data?.data) {
     try {
-      const data = JSON.parse(msg.web_app_data?.data)
+      const data = JSON.parse(msg?.web_app_data?.data)
 
       await bot.sendMessage(chatId, 'Име: ' + data?.name)
       await bot.sendMessage(chatId, 'Ваш город: ' + data?.country)
-    } catch {
+    } catch (e) {
       console.log(e)
     }
   }
